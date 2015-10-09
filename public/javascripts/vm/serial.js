da5302857c6fc6fe448b225ca2d27d660b5a4222
yejing <yejing@chunbo.com>
2015-10-09 11:54:31 +0800

    use mmState

---
+++ public/javascripts/vm/serial.js
@@ -0,0 +1,12 @@
+define(['../avalon/mmRequest'],function (avalon){
+	return Serial = {
+		index : avalon.define({
+			$id: "main-content",
+			view:'../modules/index.html',
+			title:'',
+			success:'',
+			paperBook:[]
+   });
+	}
+})
+
