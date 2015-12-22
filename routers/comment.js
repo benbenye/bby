4fe7219be3a8226cb574df00cdc7a964ec98aa24
yejing <yejing@chunbo.com>
2015-12-22 16:04:00 +0800

    统一路由命名，无效路由统一返回index.html

--- routers/comment.js
+++ routers/comment.js
@@ -2,21 +2,19 @@
 /*
  * GET comment listing.
  */
-var CommentController = require('../controllers/commentController.js'),
+var Comment = require('../controllers/commentController.js'),
 		express = require('express'),
-    CheckController = require('../controllers/checkController.js');
+    Check = require('../controllers/checkController.js');
 var comment = express.Router();
 
 	//进入评书页面
 
-	// comment.get('/user/reviewer', CheckController.checkNotLogin);
-	comment.get('/user/reviewer', CommentController.getreviewer);
+	comment.get('/api/user/reviewer', Comment.getreviewer);
 
 	//查寻书评
 	
 	//提交评书内容
-	comment.get('/book/comment', CheckController.checkNotLogin);
-	comment.get('/book/comment', CommentController.getcomments);
+	comment.get('/api/book/comment', Check.checkNotLogin, Comment.getcomments);
 
 	//进入修改评书
 	//保存修改过的书评
